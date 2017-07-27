4c3f6b0fd10139e8659409fe415b38d69beae27d
Antonio Xocoy Rosales <anxrosales@gmail.com>
2017-07-27 09:32:14 -0600

    UPDATE

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.set('view engine', 'ejs');
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './')));
 app.use(logger('dev'));
 
 app.use(function(req, res, next){
--- routes/upload.images.js
+++ routes/upload.images.js
@@ -8,19 +8,20 @@ router.post('/upload', function(req, res, next) {
     var nameFile = ""
     //Aca entra la peticion y Formidable se encarga de obtener solo el encabezado
     //"file" para asi comenzar a guardarlo
-    form.parse(req, function() {
+    form.parse(req, ()=>{
     });
     //Al estar ya en el proceso de guardado, Formidable permite modificarlo u 
     //obtener datos del archivo enviado en este caso se le indica a donde se
     //Ira a guardar y se obtiene el nombre para completar la URL de la imagen
     form.on('fileBegin', function(name, file) {
-      filePath += file.path = './public/images/' + file.name;
+      filePath += file.path = './' + file.name;
+      console.log(filePath)
       nameFile = file.name
     });
     //Al finalizar, envia la peticion devuleta ya con la URL de imagen
     //Esta sera guardada como String en la DB y asi solo se manda a llamar eso
     form.on('end', ()=> {
-      res.json({"Mensaje": fileServer+"images/"+nameFile});
+      res.json({"Mensaje": fileServer+nameFile});
       res.end();
     })
 
