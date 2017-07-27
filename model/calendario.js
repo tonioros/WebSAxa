3438ebc4c9bca0d2999e1b692cda99b74462d88e
Antonio Xocoy Rosales <anxrosales@gmail.com>
2017-07-27 11:03:24 -0600

    mejoras calendario

--- model/calendario.js
+++ model/calendario.js
@@ -3,7 +3,7 @@ var calendario = {};
 
 calendario.selectAllByCli = function(idCliente, callback) {
   if(database) {
-    database.query("SELECT ca.*, au.modelo, au.marca, au.anio, em.nombre FROM calendario ca INNER JOIN auto au ON au.idAuto = ca.idAuto INNER JOIN empresa em ON em.idEmpresa = ca.idEmpresa WHERE idCliente = ?",
+    database.query("SELECT ca.*, au.modelo, au.marca, au.anio, em.nombre, DATE_FORMAT(ca.fecha, '%Y-%m-%d') AS fechaFormat, DATE_FORMAT(ca.fecha, '%H:%i:%s') AS horaFormat FROM calendario ca INNER JOIN auto au ON au.idAuto = ca.idAuto INNER JOIN empresa em ON em.idEmpresa = ca.idEmpresa WHERE idCliente = ?",
     idCliente,
     function(error, resultados) {
       if(error) {
@@ -17,7 +17,7 @@ calendario.selectAllByCli = function(idCliente, callback) {
 
 calendario.selectAllByAut = function(idAuto, callback) {
   if(database) {
-    database.query("SELECT ca.*, au.modelo, au.marca, au.anio, em.nombre FROM calendario ca INNER JOIN auto au ON au.idAuto = ca.idAuto INNER JOIN empresa em ON em.idEmpresa = ca.idEmpresa WHERE idAuto = ?",
+    database.query("SELECT ca.*, au.modelo, au.marca, au.anio, em.nombre, DATE_FORMAT(ca.fecha, '%Y-%m-%d') AS fechaFormat, DATE_FORMAT(ca.fecha, '%H:%i:%s') AS horaFormat FROM calendario ca INNER JOIN auto au ON au.idAuto = ca.idAuto INNER JOIN empresa em ON em.idEmpresa = ca.idEmpresa WHERE idAuto = ?",
     idAuto,
     function(error, resultados) {
       if(error) {
@@ -31,7 +31,7 @@ calendario.selectAllByAut = function(idAuto, callback) {
 
 calendario.selectAllByEmp = function(idEmpresa, callback) {
   if(database) {
-    database.query("SELECT ca.*, au.modelo, au.marca, au.anio, em.nombre FROM calendario ca INNER JOIN auto au ON au.idAuto = ca.idAuto INNER JOIN empresa em ON em.idEmpresa = ca.idEmpresa WHERE idEmpresa = ?",
+    database.query("SELECT ca.*, au.modelo, au.marca, au.anio, em.nombre, DATE_FORMAT(ca.fecha, '%Y-%m-%d') AS fechaFormat, DATE_FORMAT(ca.fecha, '%H:%i:%s') AS horaFormat FROM calendario ca INNER JOIN auto au ON au.idAuto = ca.idAuto INNER JOIN empresa em ON em.idEmpresa = ca.idEmpresa WHERE idEmpresa = ?",
     idEmpresa,
     function(error, resultados) {
       if(error) {
