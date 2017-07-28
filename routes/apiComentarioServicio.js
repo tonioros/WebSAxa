d5987d308ac421c8dc50f05741e620163679acf7
Antonio Xocoy Rosales <anxrosales@gmail.com>
2017-07-28 08:53:41 -0600

    cambios comentario

--- routes/apiComentarioServicio.js
+++ routes/apiComentarioServicio.js
@@ -1,20 +1,6 @@
 var router = require("express").Router(),
     comentarioS = require("../model/comentarioservicio")
 
-router.get("/api/cs/:idComentarioS", function (req, res, next) {
-    if (req.params.idComentarioS != null) {
-        comentarioS.selectOne(req.params.idComentarioS, function (err, response) {
-            if (response != null) {
-                res.json(response)
-            } else {
-                res.json({ Mensaje: "No encontraron datos u ocurrio un error" })
-            }
-        });
-    } else {
-        res.json({ Mensaje: "ID NO VALIDO" })
-    }
-});
-
 router.get("/api/cs/serv/:idServicio", function (req, res, next) {
     if (req.params.idServicio != null) {
         comentarioS.selectAll(req.params.idServicio, function (err, response) {
@@ -29,19 +15,6 @@ router.get("/api/cs/serv/:idServicio", function (req, res, next) {
     }
 });
 
-router.get("/api/cs/cli/:idCliente", function (req, res, next) {
-    if (req.params.idCliente != null) {
-        comentarioS.selectByCli(req.params.idCliente, function (err, response) {
-            if (response != null) {
-                res.json(response)
-            } else {
-                res.json({ Mensaje: "No encontraron datos u ocurrio un error" })
-            }
-        });
-    } else {
-        res.json({ Mensaje: "ID NO VALIDO" })
-    }
-});
 
 router.post("/api/cs", function (req, res, next) {
     var data = [req.body.idServicio, req.body.idCliente, req.body.descripcion]
